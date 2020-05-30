8c486f16e9a992a03f67aabfd983abfcc3e41e68
Ivan S <ing.ivaneduardosanchez@hotmail.com>
2020-05-30 11:37:48 -0500

    Add user validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Creation" do
+    before do ## This blpock will run in every one sogle test
+      @user = User.create(email:"Test@test.com", password:"asdfasdf", password_confirmation:"asdfasdf", first_name:'test fisrt Name', last_name:'test last name') 
+      
+    end
+    it "can be created" do
+      expect(@user).to be_valid
+    end
+    it "can not be create without first_name and last_name" do
+      @user.first_name = nil
+      @user.last_name = nil
+      expect(@user).to_not be_valid 
+    end
+  end
+
 end
