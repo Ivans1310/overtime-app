381f1f7ed298ee3d4662e956e584e0534026446e
Ivan S <ing.ivaneduardosanchez@hotmail.com>
2020-05-30 10:11:56 -0500

    Delete datacleaner, change the database port, add Devise for auth

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,5 @@
+class StaticController < ApplicationController
+    def home
+        head :ok
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+# config/routes.rb
+
+Rails.application.routes.draw do
+  devise_for :users
+  root to: 'static#home'
+end
