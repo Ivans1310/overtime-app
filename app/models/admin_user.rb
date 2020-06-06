c25b70bad2cfe73fcb2f565ebe439436e3fb8a19
Ivan S <ing.ivaneduardosanchez@hotmail.com>
2020-06-06 18:04:33 -0500

    customize dashboard admin user form

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,3 +1,25 @@
 class AdminUser < User
+
+    def sign_in_count
+        
+    end
+   def current_sign_in_at
+       
+   end
+   def last_sign_in_at
+       
+   end
+   def last_sign_in_ip
+       
+   end
+
+   def current_sign_in_ip
+       
+   end
+   
+   
+   
+
+    
     
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates_presence_of :first_name, :last_name       
+
+  def sign_in_count
+
+  end
+  def current_sign_in_at
+      
+  end
+  def last_sign_in_at
+      
+  end
+  def last_sign_in_ip
+      
+  end
+
+  def current_sign_in_ip
+      
+  end
 end
