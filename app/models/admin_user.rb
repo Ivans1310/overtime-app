81210fcc996a7d92d69a6fc83a1515b3df2fe839
Ivan S <ing.ivaneduardosanchez@hotmail.com>
2020-06-13 17:02:08 -0500

    implement index view with posts

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,25 +1,19 @@
 class AdminUser < User
 
     def sign_in_count
+
+    end
+    def current_sign_in_at
         
     end
-   def current_sign_in_at
-       
-   end
-   def last_sign_in_at
-       
-   end
-   def last_sign_in_ip
-       
-   end
+    def last_sign_in_at
+        
+    end
+    def last_sign_in_ip
+        
+    end
 
-   def current_sign_in_ip
-       
-   end
-   
-   
-   
-
-    
-    
+    def current_sign_in_ip
+        
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
+@user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Ivan", last_name: "Sanchez")
+puts "1 User Created"
 100.times do |post|
-    Post.create(date: Date.today, rationale: "#{post} ratioanle content")
+    Post.create(date: Date.today, rationale: "#{post} ratioanle content", user_id: @user.id)
 end
 
 puts "100 post have been created"
